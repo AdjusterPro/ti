40b473a2f4f7005ef8c29bb033359143f444e07f
Ben Dunlap <ben@adjusterpro.com>
2021-05-11 15:14:05 -0500

    better tests //bdunlap

--- test/ti-test.rb
+++ test/ti-test.rb
@@ -2,25 +2,32 @@ require 'digest'
 
 class TiTest < Test::Unit::TestCase
     def ti
-        @ti ||= TI.new(ENV['TI'] || raise("please define TI in env (s/b API key)"), Logger.new(STDOUT), :debug => true)
+        @ti ||= TI.new(env(:TI), Logger.new(STDERR), :debug => true, :subdomain => env(:TI_TEST_SUBDOMAIN))
+    end
+
+    def env(var_sym)
+      ENV[var_sym.to_s] || raise("please define #{var_sym.to_s} in env")
     end
 
     def test_429_handling
-      20.times { self.ti.get("users/26dd1069-db08-4ee4-8575-e4693218cdd1") }
+#     omit
+      21.times { self.ti.get("users/#{env(:TI_TEST_USER)}") }
     end
 
     def test_read_profile_field
-      test_user = self.ti.get('users/26dd1069-db08-4ee4-8575-e4693218cdd1')[0]
-      assert_equal('bdunlap@agentintellect.com', test_user['email'])
+#     omit
+      test_user = self.ti.get("users/#{env(:TI_TEST_USER)}")[0]
+      assert_equal(env(:TI_TEST_USER_EMAIL), test_user['email'])
     end
 
 		def test_write_profile_field
-      test_user = self.ti.get('users/26dd1069-db08-4ee4-8575-e4693218cdd1')[0]
+#     omit
+      test_user = self.ti.get("users/#{env(:TI_TEST_USER)}")[0]
       ref_user = test_user.dup
       new_value = Digest::MD5.hexdigest(test_user['ref10'] || '')
       ref_user['ref10'] = new_value
 
-      test_user = self.ti.put('users/26dd1069-db08-4ee4-8575-e4693218cdd1', { 'email' => test_user['email'], 'ref10' => new_value })
+      test_user = self.ti.put("users/#{env(:TI_TEST_USER)}", { 'email' => test_user['email'], 'ref10' => new_value })
       ref_user.keys.reject {|k| k=='lastActiveAt' }
       .each { |k| assert_equal("#{k} = #{ref_user[k]}", "#{k} = #{test_user[k]}") }
     end
